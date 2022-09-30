060f5835e02f76e3f39924a066b84c17d32a4227
nkondd <nkond22@gmail.com>
2022-09-30 18:44:36 +0300

    feat: add artists data

---
+++ assets/js/script.js
@@ -0,0 +1,231 @@
+// Artists Data
+const artists = [
+    {
+        name: "Oleksii Sai",
+        profile_image: "assets/img/Oleksii_Sai/profile_photo.jpg",
+        text: "Війна — це такий момент, коли ти робиш вибір. Я свідомо вирішив не переривати художню практику, а просто повністю переорієнтувався на війну. Зараз роблю щось на кшталт пропаганди: плакати, постери і так далі. Намагаюся працювати якомога більше, щоб приносити користь тут і зараз всім. Віддаю багато своїх робіт на різні аукціони, які підтримують Україну." +
+            "<br><br>Зараз один і той самий сюжет часто одночасно виникає у всіх, хто робить щось подібне. Це нормально і навіть правильно, тому що це, так би мовити, направлена думка. Я не надто багато зараз думаю над своєю художньою особистістю і тим, що я маю робити як художник. Я працюю як пропагандист: що ефективно — те й годиться." +
+            "<br><br>Остання моя виставка в Росії була ще десь на початку минулого десятиліття. З тих пір я не надто уважно слідкував за російським мистецтвом і не підтримував контакт із місцевими художниками. Щоб я їм хотів сказати? Біжіть! От і все.",
+        gallery: "assets/img/Oleksii_Sai/1.jpg"
+            + "assets/img/Oleksii_Sai/2.jpg"
+            + "assets/img/Oleksii_Sai/3.jpg"
+            + "<br><br>assets/img/Oleksii_Sai/4.jpg"
+            + "assets/img/Oleksii_Sai/5.jpg"
+            + "assets/img/Oleksii_Sai/6.jpg",
+        instagram: "Instagram",
+        instagram_href: "https://www.instagram.com/oleksiysai",
+        facebook: "Facebook",
+        facebook_href: "https://www.facebook.com/liosha.say",
+        website: "Website",
+        website_href: "http://www.oleksiysai.com"
+    },
+    {
+        name: "Ihor Hora",
+        profile_image: "assets/img/Ihor_Hora/profile_photo.jpg",
+        text: "— Я розумів, що війна буде, але виявився не готовий до неї. Це не дивно. Як можна підготуватися до вибухів посеред ночі?" +
+            "<br><br>Основа колажів — воєнні фотографії з відкритих джерел. Деякі з них навіть не потребують художнього осмислення, бо самі по собі є художнім актом, як-то світлина з тілом російського військового, на яке хтось поклав гармонь. Таким фото мені лишається тільки надати виразності та зробити їх більш приємними для сприйняття. Дивитися на труп, навіть якщо це російський військовий, не дуже приємно. Але якщо надати світлині кольорів — інша справа. Створення колажів — це арттерапія, яка допомагає мені впоратися з ненавистю. Розмальовуєш мертвих ворогів, і вони перетворюються на абстрактні кольорові плями." +
+            "<br><br>На мою думку, завдання українських художників, фотографів та діячів культури сьогодні — привертати увагу світу до війни в Україні. Якщо є можливість, маємо продавати свої роботи та донатити на потреби ЗСУ. А от російська культура заслуговує лише на відміну.",
+        gallery: "assets/img/Ihor_Hora/1.jpg"
+            + "assets/img/Ihor_Hora/2.jpg"
+            + "assets/img/Ihor_Hora/3.jpg"
+            + "<br><br>assets/img/Ihor_Hora/4.jpg"
+            + "assets/img/Ihor_Hora/5.jpg"
+            + "assets/img/Ihor_Hora/6.jpg",
+        facebook: "Facebook",
+        facebook_href: "https://www.facebook.com/igorsobakka",
+    },
+    {
+        name: "Liza Yablonskaya",
+        profile_image: "assets/img/Liza_Yablonskaya/profile_photo.jpg",
+        text: "24 лютого ми з донькою були у моїх батьків у Дніпрі. Від початку я не могла робити нічого, окрім як читати новини та питати всіх знайомих, чи вони цілі. Ми заклеїли вікна скотчем, зібрали тривожні валізки, запаслися їжею та водою і спали в коридорі. Так минув тиждень. Далі мене охопив тваринний страх, що щось може статися з донькою. 1 березня я прийняла найважче рішення у житті — поїхати з України." +
+            "<br><br>Повітряна тривога, ніч, сніг та сотні червоних фар, що тягнулися на кілометри вперед, — такою вразливою, як у ту ніч, я не почувалася ніколи. Вранці ми з донькою та моєю сестрою перетнули кордон з Молдовою, а через два дні дісталися до мого друга у Кракові. Там ми прожили чотири місяці. У середині липня ми з донькою повернулися до Києва." +
+            "<br><br>Досить тривалий час було складно малювати щось на інші теми. Думати, говорити, дивитися та читати я могла лише про війну. Опинившись за кордоном, потрапляєш у паралельну реальність, де люди живуть своїм звичним життям і спокійно ходять вулицями. До цього складно звикнути. Але саме там я зрозуміла, що у світі було та є безліч людських жертв і горя, які раніше проходили повз увагу.",
+        gallery: "assets/img/Liza_Yablonskaya/1.jpg"
+            + "assets/img/Liza_Yablonskaya/2.jpg"
+            + "assets/img/Liza_Yablonskaya/3.jpg"
+            + "<br><br>assets/img/Liza_Yablonskaya/4.jpg"
+            + "assets/img/Liza_Yablonskaya/5.jpg"
+            + "assets/img/Liza_Yablonskaya/6.jpg",
+        instagram: "Instagram",
+        instagram_href: "https://www.instagram.com/yablonskali",
+        facebook: "Facebook",
+        facebook_href: "https://www.facebook.com/liza.yablonskaya",
+        website: "Website",
+        website_href: "https://lizayablonskaya.myportfolio.com"
+    },
+    {
+        name: "Sophia Suliy",
+        profile_image: "assets/img/Sophia_Suliy/profile_photo.jpg",
+        text: "У перші дні війни в мене був шалений запал. У мені кипів такий собі коктейль зі злості та агресії. Постійно хотілося працювати, реагуючи на кожен інфопривід ілюстрацією. Бувало таке: читаю новини перед сном, а потім замість того, щоб спати, починаю малювати. Гадаю, це була сублімація. Хтось пише, хтось займається фізичною працею, хтось іде воювати, а я свої переживання вимальовувала." +
+            "<br><br>На початку війни я створила відкритий архів. Роботи з нього можна було безкоштовно скачувати та використовувати. Так, мої ілюстрації демонстрували на екрані під час благодійного концерту у Варшаві. Дивлячись його, я нічого особливого не відчувала, хоча, звичайно, мені було приємно. Після події організаторка сказала, що глядачі плакали." +
+            "<br><br>Найбільше мені до вподоби ілюстрації, які я зробила для NFT-колекції. Тоді вдалося продати кілька зображень, але багато з них активно розлетілися в інстаграмі: банка томатів Drone-killer, тракторець Ukrainian Farm Forces та «Садок вишневий коло хати». Є серед улюбленців і непопулярні ілюстрації, наприклад серія Herbarium, в якій зображений кущик барбарису з гільзами. Ці роботи мінімалістичні. Можливо, вони і не принесли мені підписників, але в них є трішки про мене як іконописця.",
+        gallery: "assets/img/Sophia_Suliy/1.jpg"
+            + "assets/img/Sophia_Suliy/2.jpg"
+            + "assets/img/Sophia_Suliy/3.jpg"
+            + "<br><br>assets/img/Sophia_Suliy/4.jpg"
+            + "assets/img/Sophia_Suliy/5.jpg"
+            + "assets/img/Sophia_Suliy/6.jpg",
+        instagram: "Instagram",
+        instagram_href: "https://www.instagram.com/zosia_illustrates",
+    },
+    {
+        name: "Volodymyr Prokh",
+        profile_image: "assets/img/Volodymyr_Prokh/profile_photo.jpg",
+        text: "— Я жив у Росії деякий час, у мене в цій країні були друзі й залишилися родичі. У них там такий режим віртуальної реальності, коли навіть останній бомж стає «богоізбранним» та великодержавним. Росіянам поколіннями трамбували голову пропагандою всіх сортів. Так моя робота з унітазами і придумалася. Суспільство настільки звикло до пропагандистського лайна, що вже не може без нього жити, — копрофілія на рівні багатомільйонної країни." +
+            "<br><br>Ще задовго до цих подій я вирішив намалювати Леніна, хоч би як дивно це звучало. Мене бентежило, як якась маленька, лиса й картава людина наповнюється такою руйнівною силою, щоб залишити довжелезний кривавий слід через покоління і величезні території. У моїй уяві вона мала б роздутися до велетенського виродка, який тупо йде вперед, забираючи тисячі життів і руйнуючи все навколо. Так само сталося з Путіним та Лукашенком. А голими я їх зобразив, щоб показати неприкриту сутність цих потвор, хоч би як вони намагалися її приховати." +
+            "<br><br>У перші два тижні вторгнення, як і всі, я слідкував за новинами та адаптувався до безперервного стресу. Коли повернувся до творчості, захотів малювати більш осмислені речі про події навколо. В інших аспектах моє життя не змінилося — у Львові не було того, що пережили люди на Сході чи навіть у Києві.",
+        gallery: "assets/img/Volodymyr_Prokh/1.jpg"
+            + "assets/img/Volodymyr_Prokh/2.jpg"
+            + "assets/img/Volodymyr_Prokh/3.jpg"
+            + "<br><br>assets/img/Volodymyr_Prokh/4.jpg"
+            + "assets/img/Volodymyr_Prokh/5.jpg"
+            + "assets/img/Volodymyr_Prokh/6.jpg",
+        instagram: "Instagram",
+        instagram_href: "https://www.instagram.com/volprok",
+    },
+    {
+        name: "Stas Santimov",
+        profile_image: "assets/img/Stas_Santimov/profile_photo.jpg",
+        text: "За актуальними темами я зазвичай не женуся, але іноді вони мене бентежать, як в останні місяці, й тоді я не опираюся. Так з’явилася робота про росіян, які дивляться телевізор і запрошують дитину приєднатися, щоб вона стала такою самою, як батьки." +
+            "<br><br>Зараз українській анімації приділяють дуже багато уваги у світі. Листи з проханнями надати фільм для фестивалю надсилають так часто, що я інколи не встигаю відповідати. Нещодавно той же «Сурогат» показали у Штутгарті." +
+            "<br><br>Хоча запрошень на фестивалі багато, за нагородами я не женуся, а от нові знайомства вони допомагають завести. Це надзвичайно важливо для людей, які займаються анімацією, тому що робота дуже інтровертна, потрібно багато часу проводити за екраном.",
+        gallery: "assets/img/Stas_Santimov/1.gif"
+            + "assets/img/Stas_Santimov/2.gif"
+            + "assets/img/Stas_Santimov/3.gif"
+            + "<br><br>assets/img/Stas_Santimov/4.gif"
+            + "assets/img/Stas_Santimov/5.gif"
+            + "assets/img/Stas_Santimov/6.gif",
+        instagram: "Instagram",
+        instagram_href: "https://www.instagram.com/ssantimov",
+        facebook: "Facebook",
+        facebook_href: "https://www.facebook.com/ssantimov",
+        website: "Website",
+        website_href: "https://santimov.art"
+    },
+    {
+        name: "Kinder Album",
+        profile_image: "assets/img/Kinder_Album/profile_photo.jpg",
+        text: "Я відчула шок і заціпеніння від того, що таке взагалі можливе у нас в час, коли цивілізаційний та культурний розвиток людства не дозволяє навіть помислити про масові вбивства. Але виявилось, що ці цінності сповідують далеко не всі люди. Війна загострила відчуття того, що я лише маленька мурашка, смерть якої мало що змінить на Землі, але водночас важливішим стало те, що я можу встигнути зробити, поки ще жива.",
+        gallery: "assets/img/Kinder_Album/1.jpg"
+            + "assets/img/Kinder_Album/2.jpg"
+            + "assets/img/Kinder_Album/3.jpg"
+            + "<br><br>assets/img/Kinder_Album/4.jpg"
+            + "assets/img/Kinder_Album/5.jpg"
+            + "assets/img/Kinder_Album/6.jpg",
+        instagram: "Instagram",
+        instagram_href: "https://www.instagram.com/iamkinderalbum",
+        facebook: "Facebook",
+        facebook_href: "https://www.facebook.com/ilovekinderalbum",
+        website: "Website",
+        website_href: "https://kinder-album.com"
+    },
+    {
+        name: "Mariana Mikitiuk",
+        profile_image: "assets/img/Mariana_Mikitiuk/profile_photo.jpg",
+        text: "У перший день повномасштабного вторгнення здавалося, що це надто абсурдно, щоб тривати довго, що все закінчиться за кілька днів, бо такого не може бути зараз посеред Європи. Згодом стало зрозуміло, що може бути що завгодно, і ядерна війна — це цілком собі варіант. Треба готуватися до всього і намагатися повернутись до якоїсь подоби нормального життя." +
+            "<br><br>Війна загострила відчуття вдячності. Уже нічого не вдається сприймати як належне. Щодня, коли вмикаю воду, світло чи купую їжу, думаю про людей, які роблять це можливим." +
+            "<br><br>У спокійний стабільний настрій досі важко втрапити: частіше емоції або викручені на максимум, або за щільною пеленою апатії. Малювання допомагає відчути трохи контролю і користі від себе. Мені здається, ми зараз у процесі ще одного культурного відродження, і сподіваюсь, цього разу ніщо не завадить йому.",
+        gallery: "assets/img/Mariana_Mikitiuk/1.jpg"
+            + "assets/img/Mariana_Mikitiuk/2.jpg"
+            + "assets/img/Mariana_Mikitiuk/3.jpg"
+            + "<br><br>assets/img/Mariana_Mikitiuk/4.jpg"
+            + "assets/img/Mariana_Mikitiuk/5.jpg"
+            + "assets/img/Mariana_Mikitiuk/6.jpg",
+        instagram: "Instagram",
+        instagram_href: "https://www.instagram.com/mariana.mikitiuk",
+        facebook: "Facebook",
+        facebook_href: "https://www.facebook.com/mariana.mikitiuk",
+    },
+    {
+        'Yulia Danilevskaya':
+        {
+            name: "Yulia Danilevskaya",
+            profile_image: "assets/img/Other/yulia_danilevskaya/profile_photo.jpg",
+            gallery: "assets/img/Other/yulia_danilevskaya/1.jpg"
+                + "assets/img/Other/yulia_danilevskaya/2.jpg"
+                + "assets/img/Other/yulia_danilevskaya/3.jpg",
+            instagram: "Instagram",
+            instagram_href: "https://www.instagram.com/yulia_danilevskaya",
+        },
+        'Sasha Barbolin':
+        {
+            name: "Sasha Barbolin",
+            profile_image: "assets/img/Other/Sasha_Barbolin/profile_photo.jpg",
+            gallery: "assets/img/Other/Sasha_Barbolin/1.jpg"
+                + "assets/img/Other/Sasha_Barbolin/2.jpg"
+                + "assets/img/Other/Sasha_Barbolin/3.jpg",
+            instagram: "Instagram",
+            instagram_href: "https://www.instagram.com/bolin132",
+        },
+        'Denysenkov Iurii':
+        {
+            name: "Denysenkov Iurii",
+            profile_image: "assets/img/Other/Denysenkov_Iurii/profile_photo.jpg",
+            gallery: "assets/img/Other/Denysenkov_Iurii/1.jpg"
+                + "assets/img/Other/Denysenkov_Iurii/2.jpg"
+                + "assets/img/Other/Denysenkov_Iurii/3.jpg",
+            instagram: "Instagram",
+            instagram_href: "https://www.instagram.com/juraden",
+        },
+        'Kita Diena':
+        {
+            name: "Kita Diena",
+            profile_image: "assets/img/Other/Kita_Diena/profile_photo.jpg",
+            gallery: "assets/img/Other/Kita_Diena/1.jpg"
+                + "assets/img/Other/Kita_Diena/2.jpg"
+                + "assets/img/Other/Kita_Diena/3.jpg",
+            instagram: "Instagram",
+            instagram_href: "https://www.instagram.com/kitadiena",
+        },
+        'NEIVANMADE':
+        {
+            name: "NEIVANMADE",
+            profile_image: "assets/img/Other/neivanmade/profile_photo.jpg",
+            gallery: "assets/img/Other/neivanmade/1.jpg"
+                + "assets/img/Other/neivanmade/2.jpg"
+                + "assets/img/Other/neivanmade/3.jpg",
+            instagram: "Instagram",
+            instagram_href: "https://www.instagram.com/neivanmade",
+        },
+        'Ksenia Igolkina':
+        {
+            name: "Ksenia Igolkina",
+            profile_image: "assets/img/Other/Ksenia_Igolkina/profile_photo.jpg",
+            gallery: "assets/img/Other/Ksenia_Igolkina/1.jpg"
+                + "assets/img/Other/Ksenia_Igolkina/2.jpg"
+                + "assets/img/Other/Ksenia_Igolkina/3.jpg",
+            instagram: "Instagram",
+            instagram_href: "https://www.instagram.com/iks.art",
+        },
+        'bol.no.nonono':
+        {
+            name: "bol.no.nonono",
+            profile_image: "assets/img/Other/bol.no.nonono/profile_photo.jpg",
+            gallery: "assets/img/Other/bol.no.nonono/1.jpg"
+                + "assets/img/Other/bol.no.nonono/2.jpg"
+                + "assets/img/Other/bol.no.nonono/3.jpg",
+            instagram: "Instagram",
+            instagram_href: "https://www.instagram.com/bol.no.nonono",
+        },
+        'Kateryna Kosheleva':
+        {
+            name: "Kateryna Kosheleva",
+            profile_image: "assets/img/Other/Kateryna_Kosheleva/profile_photo.jpg",
+            gallery: "assets/img/Other/Kateryna_Kosheleva/1.jpg"
+                + "assets/img/Other/Kateryna_Kosheleva/2.jpg"
+                + "assets/img/Other/Kateryna_Kosheleva/3.jpg",
+            instagram: "Instagram",
+            instagram_href: "https://www.instagram.com/tokkamak",
+        },
+        'Marta Koshulinska':
+        {
+            name: "Marta Koshulinska",
+            profile_image: "assets/img/Other/Marta_Koshulinska/profile_photo.jpg",
+            gallery: "assets/img/Other/Marta_Koshulinska/1.jpg"
+                + "assets/img/Other/Marta_Koshulinska/2.jpg"
+                + "assets/img/Other/Marta_Koshulinska/3.jpg",
+            instagram: "Instagram",
+            instagram_href: "https://www.instagram.com/marta_koshulinska",
+        },
+    },
+];
